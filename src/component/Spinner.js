3b2ad6e4944f8153d1f174d7ed8c42d604fb56c4
Dhanji Sharma <kdhanji183@gmail.com>
2023-07-30 08:25:31 +0530

    adding newsmonkey

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -4,9 +4,7 @@ export default class Spinner extends Component {
   render() {
     return (
       <div className='text-center my-3'>
-        <span style={{color:"white"}}>Fetching</span>
         <img src={Loading} alt="loading" />
-        <span style={{color:"white"}}>Data</span>
       </div>
     )
   }
